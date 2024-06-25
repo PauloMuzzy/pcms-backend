10963a97bb2e34ed1e8fb7cfc656db301b04f2ab
PauloMuzzy <paulokriger@gmail.com>
2024-06-24 23:14:50 -0300

    fix: corrigido texto class validator

--- src/modules/patients/dto/create-patient-request.dto.ts
+++ src/modules/patients/dto/create-patient-request.dto.ts
@@ -10,74 +10,79 @@ import {
 
 export class CreatePatientRequestDto {
   @ApiProperty()
-  @IsNotEmpty({ message: 'Nome não pode ser vazio' })
-  @IsString({ message: 'Nome deve ser uma string' })
-  @Length(1, 500, { message: 'Nome deve ter entre 1 e 500 caracteres' })
+  @IsNotEmpty({ message: 'O nome não pode estar vazio' })
+  @IsString({ message: 'O nome deve ser uma string' })
+  @Length(1, 500, { message: 'O nome deve ter entre 1 e 500 caracteres' })
   name: string;
 
   @ApiProperty()
-  @IsNotEmpty({ message: 'Sobrenome não pode ser vazio' })
-  @IsString({ message: 'Sobrenome deve ser uma string' })
-  @Length(1, 500, { message: 'Sobrenome deve ter entre 1 e 500 caracteres' })
+  @IsNotEmpty({ message: 'O sobrenome não pode estar vazio' })
+  @IsString({ message: 'O sobrenome deve ser uma string' })
+  @Length(1, 500, { message: 'O sobrenome deve ter entre 1 e 500 caracteres' })
   lastName: string;
 
   @ApiProperty()
-  @IsNotEmpty({ message: 'CPF não pode ser vazio' })
-  @IsNumber({}, { message: 'CPF deve ser um número' })
-  @Length(1, 11, { message: 'CPF deve ter 11 caracteres' })
+  @IsNotEmpty({ message: 'O CPF não pode estar vazio' })
+  @IsString({ message: 'O CPF deve ser uma string' })
+  @Length(11, 11, { message: 'O CPF deve ter 11 caracteres' })
   cpf: string;
 
   @ApiProperty()
-  @IsNotEmpty({ message: 'Email não pode ser vazio' })
-  @IsEmail({}, { message: 'Email deve ser um email válido' })
+  @IsNotEmpty({ message: 'O email não pode estar vazio' })
+  @IsEmail({}, { message: 'O email deve ser um email válido' })
   email: string;
 
   @ApiProperty()
-  @IsNotEmpty({ message: 'Data de nascimento não pode ser vazia' })
-  @IsDateString({}, { message: 'Data de nascimento deve ser uma data válida' })
+  @IsNotEmpty({ message: 'A data de nascimento não pode estar vazia' })
+  @IsDateString(
+    {},
+    { message: 'A data de nascimento deve ser uma data válida' },
+  )
   dateOfBirth: string;
 
   @ApiProperty()
-  @IsNotEmpty({ message: 'Gênero não pode ser vazio' })
-  @IsNumber({}, { message: 'Gênero deve ser um número' })
+  @IsNotEmpty({ message: 'O gênero não pode estar vazio' })
+  @IsNumber({}, { message: 'O gênero deve ser um número' })
   gender: number;
 
   @ApiProperty()
-  @IsNotEmpty({ message: 'Profissão não pode ser vazia' })
-  @IsNumber({}, { message: 'Profissão deve ser um número' })
+  @IsNotEmpty({ message: 'A profissão não pode estar vazia' })
+  @IsNumber({}, { message: 'A profissão deve ser um número' })
   profession: number;
 
   @ApiProperty()
-  @IsNotEmpty({ message: 'Endereço não pode ser vazio' })
-  @IsString({ message: 'Endereço deve ser uma string' })
+  @IsNotEmpty({ message: 'O telefone não pode estar vazio' })
+  @IsString({ message: 'O telefone deve ser uma string' })
   phone: string;
 
   @ApiProperty()
-  @IsNotEmpty({ message: 'CEP não pode ser vazio' })
-  @IsString({ message: 'CEP deve ser uma string' })
+  @IsNotEmpty({
+    message: 'O nome do contato de emergência não pode estar vazio',
+  })
+  @IsString({ message: 'O nome do contato de emergência deve ser uma string' })
   emergencyContactName: string;
 
   @ApiProperty()
   @IsNotEmpty({
-    message: 'Telefone do contato de emergência não pode ser vazio',
+    message: 'O telefone do contato de emergência não pode estar vazio',
   })
   @IsString({
-    message: 'Telefone do contato de emergência deve ser uma string',
+    message: 'O telefone do contato de emergência deve ser uma string',
   })
   emergencyContactPhone: string;
 
   @ApiProperty({ example: 1 })
   @IsNotEmpty({
-    message: 'Parentesco do contato de emergência não pode ser vazio',
+    message: 'O parentesco do contato de emergência não pode estar vazio',
   })
   @IsNumber(
     {},
-    { message: 'Parentesco do contato de emergência deve ser um número' },
+    { message: 'O parentesco do contato de emergência deve ser um número' },
   )
   emergencyContactRelationship: number;
 
   @ApiProperty()
-  @IsNotEmpty({ message: 'CEP não pode ser vazio' })
-  @IsNumber({}, { message: 'CEP deve ser um número' })
+  @IsNotEmpty({ message: 'O status ativo não pode estar vazio' })
+  @IsNumber({}, { message: 'O status ativo deve ser um número' })
   active: number;
 }
